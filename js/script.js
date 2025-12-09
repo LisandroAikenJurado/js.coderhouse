db0cf0444d16dab6b94caf2692bac193933eed59
LisandroAikenJurado <lisandrojuradoml@gmail.com>
2025-12-09 12:26:37 -0300

    primeras notas

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// conceptos generales de JavaScript
+let nombre = "Juan"; // variable de tipo string
+let edad = 30; // variable de tipo number
+let esEstudiante = true; // variable de tipo boolean
+const PI = 3.1416; // constante
+
+//pedir datos al usuario
+let ciudad = prompt("Ingrese su ciudad:"); //prompt siempre devuelve datos de tipo string
+
+// si quiero que el dato sea de otro tipo, debo convertirlo
+let edadUsuario = Number(prompt("Ingrese su edad:")); //convertir a number
+
+
+//mostrar datos en consola
+
+
+console.log("Ciudad ingresada: " + ciudad);
